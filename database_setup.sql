aa183dce5d5c53412b2e4323d47f76193499ddf2
AQUILA2225 <160770287+AQUILA2225@users.noreply.github.com>
2026-03-11 12:45:49 +0530

    Update database_setup.sql

--- database_setup.sql
+++ database_setup.sql
@@ -1,1 +1,20 @@
+# Cretae Database (First create a database to store your tables)
+  CREATE DATABASE database_name;
 
+# View Available Databases (This command shows all databases present in the system)
+ SHOW DATABASES;
+
+# Select the Database (Before creating tables, we need to select the database)
+ USE database_name;
+
+# Create a Table (Create a table with column name, data_type and different constraints)
+ EX: CREATE TABLE student (
+    id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    stream VARCHAR(25) DEFAULT 'CSE',
+    role ENUM('student','trainer','admin')
+);
+
+NOTE: Every query must end with semicolon.
+  
